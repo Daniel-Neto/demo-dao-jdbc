02cf011e20712dc6afb69f5ce8e754a53627f179
Daniel Almeida <dasn@ecomp.poli.br>
2024-02-17 13:10:30 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,16 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1,"Books");
-		System.out.println(obj);
-	
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // instancia a implementação de sellerDao usando
 														    // a instância SellerDaoJDBC. Desse modo, vai pegar
 															// as operações implementadas em SellerDaoJDBC.
 		// 
 		//Desse modo o meu programa principal não conhece a implementação da classe SellerDaoJDBC.
-		
+		System.out.println("==== Teste 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
